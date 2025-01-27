f348b4539214d6f028396d0659adfd5c900b673b
Ahmed Hanzala <ahmedhanzala2020@gmail.com>
2025-01-27 22:48:39 +0500

    updated store

--- crud-app/src/store.js
+++ crud-app/src/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import studentsReducer from './features/studentsSlice';
-import classesReducer from './features/classesSlice';
-import sectionsReducer from './features/sectionsSlice';
+// import classesReducer from './features/classesSlice';
+// import sectionsReducer from './features/sectionsSlice';
 
 export const store = configureStore({
   reducer: {
     students: studentsReducer,
-    classes: classesReducer,
-    sections: sectionsReducer,
+    // classes: classesReducer,
+    // sections: sectionsReducer,
   },
 });
